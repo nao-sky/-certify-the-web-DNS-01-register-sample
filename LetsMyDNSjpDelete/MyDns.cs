98d16ccc097037cf5d23b8a450096bba38aea896
naomasa <47715476+nao-sky@users.noreply.github.com>
2019-03-23 20:30:34 +0900

    Add files via upload

---
+++ LetsMyDNSjpDelete/MyDns.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LetsMyDNSjp
+{
+    public class MyDNSJP
+    {
+        public string MYDNSJP_URL = "http://www.mydns.jp/directedit.html";
+        public string MYDNSJP_MASTERID = "yourmasterid";
+        public string MYDNSJP_MASTERPWD = "yourpasswd";
+        public string MYDNSJP_DOMAIN = "yourdomain";
+
+        public string CERTBOT_DOMAIN;
+        public string CERTBOT_VALIDATION;
+
+        public void REGISTAsync()
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic["EDIT_CMD"] = "DELETE";
+            dic["CERTBOT_DOMAIN"] = MYDNSJP_DOMAIN;
+            dic["CERTBOT_VALIDATION"] = this.CERTBOT_VALIDATION;
+
+            var content = new FormUrlEncodedContent(dic);
+
+            HttpRequestMessage mes = new HttpRequestMessage();
+            mes.Headers.Authorization = new AuthenticationHeaderValue("Basic", this.BuildAccout());
+            mes.Method = HttpMethod.Post;
+            mes.Content = content;
+            mes.RequestUri = new Uri(this.MYDNSJP_URL);
+
+            using (HttpClient client = new HttpClient())
+            {
+
+                Task<HttpResponseMessage> task = client.SendAsync(mes);
+
+                task.Wait();
+
+            }
+        }
+
+
+        private string BuildAccout()
+        {
+            byte[] src = new UTF8Encoding().GetBytes(this.MYDNSJP_MASTERID + ":" + MYDNSJP_MASTERPWD);
+            string dest = Convert.ToBase64String(src);
+            string ret = Regex.Replace(dest, "-", String.Empty);
+            return ret;
+        }
+    }
+}
---
+++ LetsMyDNSjpDelete/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System;
+using System.Reflection;
+using System.IO;
+using System.Collections.Generic;
+using LetsMyDNSjp;
+
+namespace LetsDNSDelete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string loc = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+
+            using (StreamReader sr = new StreamReader(loc + "\\config"))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.StartsWith("#"))
+                        continue;
+
+                    string[] rec = line.Split(':');
+                    dic[rec[0].Trim()] = rec[1].Trim();
+                }
+            }
+
+            MyDNSJP myDns = new MyDNSJP();
+
+            myDns.MYDNSJP_MASTERID = dic["MYDNSJP_MASTERID"];
+            myDns.MYDNSJP_MASTERPWD = dic["MYDNSJP_MASTERPWD"];
+            myDns.MYDNSJP_DOMAIN = dic["MYDNSJP_DOMAIN"];
+
+            myDns.CERTBOT_DOMAIN = args[0];
+            myDns.CERTBOT_VALIDATION = args[2];
+
+            myDns.REGISTAsync();
+
+            int idx = 0;
+            foreach (var s in args)
+                Console.WriteLine($"arg{idx++} : {s}");
+
+        }
+    }
+}
